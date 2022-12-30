258955e6d8365cdd544ff572f3a1b2f7bf60ef55
aliabar <114748399+aliabar@users.noreply.github.com>
2022-12-30 12:19:43 -0800

    first commit

---
+++ ajs.js
@@ -0,0 +1,126 @@
+function clic(){
+    document.getElementById("bdf").style.width="210px";
+    document.getElementById("clic1").style.display="none";
+    document.getElementById("xz").style.display="block";
+    document.getElementById("xz1").style.display="block";
+    document.getElementById("xz2").style.display="block";
+    document.getElementById("xz3").style.display="block";
+    document.getElementById("xz4").style.display="block";
+    document.getElementById("xz5").style.display="block";
+    document.getElementById("xz6").style.display="block";
+    document.getElementById("xz7").style.display="block";
+    document.getElementById("xz8").style.display="block";
+    document.getElementById("xz9").style.display="block";
+    document.getElementById("cvd1").style.display="block";
+
+}
+
+function cvd(){
+    document.getElementById("bdf").style.width="72px";
+    document.getElementById("clic1").style.display="block";
+    document.getElementById("xz").style.display="none";
+    document.getElementById("xz1").style.display="none";
+    document.getElementById("xz2").style.display="none";
+    document.getElementById("xz3").style.display="none";
+    document.getElementById("xz4").style.display="none";
+    document.getElementById("xz5").style.display="none";
+    document.getElementById("xz6").style.display="none";
+    document.getElementById("xz7").style.display="none";
+    document.getElementById("xz8").style.display="none";
+    document.getElementById("xz9").style.display="none";
+    document.getElementById("cvd1").style.display="none";
+}
+
+
+function openxz() {
+    document.getElementById("clx").style.width="0px";
+}
+function clio() {
+    document.getElementById("clx").style.width="250px";
+}
+
+
+
+function clroeb() {
+    document.getElementById("navbar").style.backgroundColor="rgb(63, 60, 67)";
+    document.getElementById("ablac").style.backgroundColor="rgb(63, 60, 67)";
+    document.getElementById("br").style.display="block";
+    document.getElementById("br11").style.display="none";
+    document.getElementById("br22").style.display="none";
+    document.getElementById("br33").style.display="none";
+    document.getElementById("br2").style.display="none";
+    document.getElementById("br3").style.display="none";
+}
+function clroeb2() {
+    document.getElementById("navbar").style.backgroundColor="rgb(92, 191, 92)";
+    document.getElementById("ablac").style.backgroundColor="rgb(92, 191, 92)";
+    document.getElementById("br").style.display="none";
+    document.getElementById("br11").style.display="none";
+    document.getElementById("br22").style.display="none";
+    document.getElementById("br33").style.display="none";
+    document.getElementById("br2").style.display="block";
+    document.getElementById("br3").style.display="none";
+
+}
+function clroeb3() {
+    document.getElementById("navbar").style.backgroundColor=" rgb(213, 82, 82)";
+    document.getElementById("ablac").style.backgroundColor=" rgb(213, 82, 82)";
+    document.getElementById("br").style.display="none";
+    document.getElementById("br11").style.display="none";
+    document.getElementById("br22").style.display="none";
+    document.getElementById("br33").style.display="none";
+    document.getElementById("br2").style.display="none";
+    document.getElementById("br3").style.display="block";
+
+}
+function clroeb11() {
+    document.getElementById("navbar").style.backgroundColor="rgb(82, 178, 213)";
+    document.getElementById("ablac").style.backgroundColor="rgb(82, 178, 213)";
+    document.getElementById("br").style.display="none";
+    document.getElementById("br2").style.display="none";
+    document.getElementById("br3").style.display="none";
+    document.getElementById("br11").style.display="block";
+    document.getElementById("br22").style.display="none";
+    document.getElementById("br33").style.display="none";
+
+}
+function clroeb22() {
+    document.getElementById("navbar").style.backgroundColor=" rgb(213, 82, 198)";
+    document.getElementById("ablac").style.backgroundColor=" rgb(213, 82, 198)";
+    document.getElementById("br").style.display="none";
+    document.getElementById("br2").style.display="none";
+    document.getElementById("br3").style.display="none";
+    document.getElementById("br11").style.display="none";
+    document.getElementById("br22").style.display="block";
+    document.getElementById("br33").style.display="none";
+
+}
+function clroeb33() {
+    document.getElementById("navbar").style.backgroundColor="rgb(77, 112, 165)";
+    document.getElementById("ablac").style.backgroundColor="rgb(77, 112, 165)";
+    document.getElementById("br").style.display="none";
+    document.getElementById("br2").style.display="none";
+    document.getElementById("br3").style.display="none";
+    document.getElementById("br11").style.display="none";
+    document.getElementById("br22").style.display="none";
+    document.getElementById("br33").style.display="block";
+
+}
+
+
+
+
+
+
+function za() {
+    document.getElementById("navbar").style.backgroundColor="rgb(63, 60, 67)";
+    document.getElementById("ablac").style.backgroundColor="rgb(63, 60, 67)";
+    document.getElementById("az1").style.display="block";
+    document.getElementById("co").style.backgroundColor="rgb(63, 60, 67)";
+    document.getElementById("br").style.display="none";
+    document.getElementById("br11").style.display="none";
+    document.getElementById("br22").style.display="none";
+    document.getElementById("br33").style.display="none";
+    document.getElementById("br2").style.display="none";
+    document.getElementById("br3").style.display="none";
+}
